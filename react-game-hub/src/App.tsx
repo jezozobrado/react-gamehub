4b50e191f6339c79e2d4f26d8d85ddd66db6cb36
jezozobrado <124932574+jezozobrado@users.noreply.github.com>
2023-04-20 01:04:12 +0800

    Revert "Fetch the games."
    This reverts commit 4f74ccf11d02e5811a001218706a7839e19ecb95.

--- react-game-hub/src/App.tsx
+++ react-game-hub/src/App.tsx
@@ -1,6 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
-import GameGrid from "./components/GameGrid";
 
 const App = () => {
   return (
@@ -16,9 +15,7 @@ const App = () => {
       <Show above="lg">
         <GridItem area="aside">Aside</GridItem>
       </Show>
-      <GridItem area="main">
-        <GameGrid />
-      </GridItem>
+      <GridItem area="main">Main</GridItem>
     </Grid>
   );
 };
--- react-game-hub/src/services/api-client.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://api.rawg.io/api/",
-  params: {
-    key: "47aad382c27941fb98199f74fac8116e",
-  },
-});
