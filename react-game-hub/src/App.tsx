a6c25ec324bfe1f7b0055c34d4a0db4168de4ba3
jezozobrado <124932574+jezozobrado@users.noreply.github.com>
2023-04-20 00:22:42 +0800

    Rebuild layout.

--- react-game-hub/src/App.tsx
+++ react-game-hub/src/App.tsx
@@ -1,10 +1,25 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
   return (
-    <div>
-      <Button colorScheme="blue">Button</Button>
-    </div>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="blue">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="red">
+        Main
+      </GridItem>
+    </Grid>
   );
 };
 
