3ec9df02d38f42e9229ea61913c4e0ef5158c330
jezozobrado <124932574+jezozobrado@users.noreply.github.com>
2023-04-20 17:53:26 +0800

    Revert "Create a generic data fetching hook."
    This reverts commit 1cb569c26e910ca0867cf26118c9208ac19b1ce3.

--- react-game-hub/src/components/GameGrid.tsx
+++ react-game-hub/src/components/GameGrid.tsx
@@ -5,7 +5,7 @@ import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-  const { data, error, isLoading } = useGames();
+  const { games, error, isLoading } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
@@ -18,13 +18,13 @@ const GameGrid = () => {
       >
         {isLoading &&
           skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton />
+            <GameCardContainer>
+              <GameCardSkeleton key={skeleton} />
             </GameCardContainer>
           ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
+        {games.map((game) => (
+          <GameCardContainer>
+            <GameCard key={game.id} game={game} />
           </GameCardContainer>
         ))}
       </SimpleGrid>
--- react-game-hub/src/hooks/useGenres.ts
+++
@@ -1,9 +0,0 @@
-import useData from "./useData";
-
-export interface Genre {
-  id: number;
-  name: string;
-}
-
-const useGenres = () => useData<Genre>("/genres");
-export default useGenres;
